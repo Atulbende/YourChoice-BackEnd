762c8b0ddc5e76778a8c40fe68052c087c89cc27
Atulbende <107322576+Atulbende@users.noreply.github.com>
2023-11-07 23:39:30 +0530

    package

--- src/server.js
+++ src/server.js
@@ -2,20 +2,39 @@
 const express =require('express');
 const mysql = require('mysql');
 const cors= require('cors');
+const bcrypt = require('bcrypt');
 const app= express();
 app.use(express.json());
 app.use(cors());
+const Salt=10;
 const db = mysql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'SAVtech',
-        database:'dashboard'
-});
+            host:'localhost',
+            user:'root',
+            password:'SAVtech',
+            database:'dashboard'
+        });
+    app.post('/login',(req,res)=>{
+        db.query('Select * from users where UserName=?',[req.body.Login_UserName],(err,result)=>{
+            if(result.length>0){
+                bcrypt.compare(req.body.Login_Password,result[0].Password,(err,invalidate)=>{            
+                res.send(invalidate);
+            });
+        }else{
+            res.send(false);
+        }
+        });
+    })
     app.post('/signup',(req,res)=>{
-        // console.log([JSON.stringify(req.body)]);
-    db.query(`call SignupSave(?,?,?,@Per_Result)`,[req.body.Signup_Email,req.body.Signup_UserName,req.body.Signup_Password],(error,result)=>{    
-        if(error===null){res.send(result[0][0].Per_Result);}else {res.send("Access denied");}
-     });
+        let str=req.body.Signup_Password.toString();
+        bcrypt.hash(str, 10, (err, hash) => {
+            if (err) {
+              console.error('Error hashing password:', err);
+              return;
+            }
+            db.query(`call SignupSave(?,?,?,@Per_Result)`,[req.body.Signup_Email,req.body.Signup_UserName,hash],(error,result)=>{   
+                if(error===null){res.send(result[0][0].Per_Result);}else {res.send("Access denied");}
+             })
+          });
     })
 app.listen(3001,()=>{
     console.log('Connect');
