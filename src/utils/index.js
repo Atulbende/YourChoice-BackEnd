1daf9f57526112a84844a6807638f33e21f0c0ba
Atulbende <107322576+Atulbende@users.noreply.github.com>
2024-03-26 16:06:40 +0530

    utils function created

---
+++ src/utils/index.js
@@ -0,0 +1,27 @@
+import {responseCodes} from '../config/dbStatusCode.js';
+const sanitizeResponse=(_result)=>{
+    if(Array.isArray(_result[0])){
+      return _result[0];
+    }else{
+      return _result;
+    }
+  }
+const getNewObj=(_obj)=>{
+  const newObj={};
+  _obj.forEach(_v => {
+    newObj[_v?.COLUMN_NAME]=_v?.COLUMN_NAME==='Pid'?'-1':'';
+  });
+  return [newObj]
+}
+const com_message=(id)=>{
+  
+
+  return responseCodes.find((_v)=>Number(_v.id)===Number(id));
+}
+
+  export 
+  {
+    sanitizeResponse,
+    getNewObj,
+    com_message
+  }
