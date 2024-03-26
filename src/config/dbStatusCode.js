af4c4126584a003391cd1ceb234fbfb3628d0bea
Atulbende <107322576+Atulbende@users.noreply.github.com>
2024-03-26 16:07:34 +0530

    status code file created

---
+++ src/config/dbStatusCode.js
@@ -0,0 +1,8 @@
+  const responseCodes=[
+    {id:1,Msg:'Record save successfully!'},
+    {id:2,Msg:'Record updated successfully!'},
+    {id:-1,Msg:'Record already exist!'},
+    {id:-2,Msg:'Unable to update record because User Name already exist!'}
+];
+
+export {responseCodes}
