df1fed140b06b82c3264124ea43f756790dd47f4
Atulbende <107322576+Atulbende@users.noreply.github.com>
2024-04-22 00:36:46 +0530

    some allow headers middlware added

--- src/app.js
+++ src/app.js
@@ -4,11 +4,17 @@ import { CookiesOptions } from "./config/cookiesConfig.js";
 import cookieParser from "cookie-parser";
 const app = express();
 const corsOptions = {
-    origin: ['http://127.0.0.1:3000', 'http://localhost:3000','https://your-choice-front-end-git-master-atul-s-projects-b035b0ba.vercel.app'],
+    origin: ['http://127.0.0.1:3000','http://localhost:3002','http://localhost:3000','https://your-choice-front-end-git-master-atul-s-projects-b035b0ba.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization','AppId'], // Specify the allowed headers
     credentials: true // Enable credentials (cookies, authorization headers, etc)
 };
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // or specify specific origins
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 app.use(cors(corsOptions));
 app.use(cookieParser(CookiesOptions));
 app.use(express.json());
